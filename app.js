c5febe573f66da4bf6aa1cea8671aaceed14c96a
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-08 14:55:06 +0300

    typing

---
+++ app.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach((entry)=>{
+    // console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show')
+    }
+    else{
+        entry.target.classList.remove('show')
+    }
+})
+
+
+})
+const hidden_element= document.querySelectorAll(".hidden")
+
+hidden_element.forEach( (element)=>observer.observe(element) )
+
+
+// window.matchMedia('(prefers-reduced-motion: reduce)');
